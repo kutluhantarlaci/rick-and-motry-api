0d499eb595e885a67162c45f5d9a712f9474f856
kutlu <kutlutarlaci@gmail.com>
2023-09-27 23:43:15 +0300

    css eklendi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
+'use client'
+import Link from 'next/link'
+
 export default function Header() {
     return (
         <div className={'header'}>
-            <span>Docs</span>
-            <span>About</span>
-            <span>SUPPORT US</span>
+            <Link href={'https://rickandmortyapi.com/documentation'} className={'hover'}>Docs</Link>
+            <Link href={'https://rickandmortyapi.com/about'} className={'hover'}>About</Link>
+            <Link href={'https://rickandmortyapi.com/support-us'} className={'hover'}>SUPPORT US</Link>
         </div>
     )
 }
